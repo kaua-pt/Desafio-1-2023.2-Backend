d5d1f2e75a1d43bbdad93abc349580b9876c5ccd
Kauã Vinícius <kauaponte@hotmail.com>
2023-10-17 20:50:28 -0300

    Adição da cifra de césar

---
+++ CaesarsCipher/CaesarsCipher.js
@@ -0,0 +1,16 @@
+function rot13(str) {
+    let ans = ""
+    for(let i = 0; i < str.length; i++) {
+        let aC = str.charCodeAt(i)
+        if(aC <= 77 && aC >=65)
+            ans+= String.fromCharCode(aC+13)
+        else if(aC >= 78 && aC <=90)
+            ans += String.fromCharCode(aC-13)
+        else 
+          ans += str[i]
+    }
+    console.log(ans)
+    return ans;
+}
+
+rot13("SERR PBQR PNZC");
