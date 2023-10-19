d170813cd6d8a39636b9ea6e99b6e939f2938615
Kauã Vinícius <kauaponte@hotmail.com>
2023-10-18 23:13:47 -0300

    Iniciando RomanNumeralConverter

---
+++ RomanNumeralConverter/RomanNumeralConverter.js
@@ -0,0 +1,14 @@
+function convertToRoman(num) {
+    // criar tabela
+    const numbers = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
+                 "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
+                 "","I","II","III","IV","V","VI","VII","VIII","IX"]
+  
+    // para cada caracter, adicionar o número a direita
+    for(c in num){
+  
+    }
+   return num;
+  }
+  
+  convertToRoman(36);
