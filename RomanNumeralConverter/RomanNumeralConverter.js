da79982af3efe1dac1c64a28f0f74efc36f58a8b
Kauã Vinícius <kauaponte@hotmail.com>
2023-10-19 19:06:34 -0300

    Adição do resto da lógica

--- RomanNumeralConverter/RomanNumeralConverter.js
+++ RomanNumeralConverter/RomanNumeralConverter.js
@@ -1,14 +1,20 @@
 function convertToRoman(num) {
-    // criar tabela
-    const numbers = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
-                 "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
-                 "","I","II","III","IV","V","VI","VII","VIII","IX"]
-  
-    // para cada caracter, adicionar o número a direita
-    for(c in num){
-  
+    const numbers = [
+              ["","M","MM","MMM"],
+              ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"],
+              ["","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",],
+              ["","I","II","III","IV","V","VI","VII","VIII","IX"]
+          ];
+
+    let ans = "";
+    num = num.toString();
+    let a = 4 - num.length;
+
+    for(let i = 0; i < num.length;i++){
+        ans += numbers[a + i][parseInt(num[i],10)];
     }
-   return num;
-  }
-  
-  convertToRoman(36);
+
+    return ans;
+}
+
+convertToRoman(36);
