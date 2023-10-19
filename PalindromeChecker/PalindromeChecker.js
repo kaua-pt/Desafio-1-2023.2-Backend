5c47f2bdfaf723ab1ad8b280dc806ad21aba5dc1
Kauã Vinícius <kauaponte@hotmail.com>
2023-10-18 21:15:35 -0300

    Adição da questão do palindromo completa

---
+++ PalindromeChecker/PalindromeChecker.js
@@ -0,0 +1,16 @@
+function palindrome(str) {
+
+    const re = /[A-Z]||[\d]/gi
+    str = str.match(re).join("").toLowerCase()
+    console.log(str)
+
+    let N = str.length;
+    for (let i = 0; i < N / 2; ++i) {
+        if (str[i]!= str[N - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+}
+  
+palindrome("eye");
